192211a1d286ce45696a651256726a0fcf2415a3
chaitanya-makkapati-ITS2 <chaitanya.makkapati@its.ny.gov>
2024-12-12 12:04:20 -0500

    week2 commit

---
+++ week2/week2/week2App.swift
@@ -0,0 +1,17 @@
+//
+//  week2App.swift
+//  week2
+//
+//  Created by Chaitanya Makkapati on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
